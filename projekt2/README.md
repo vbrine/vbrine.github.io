6af3fbe4af6cbe36f00c533807163a0f581c7fe2
unknown <tanulo@t12g14.hunyadi.local>
2022-11-08 12:06:10 +0100

    projekt feltöltés

---
+++ projekt2/README.md
@@ -0,0 +1,1 @@
+# RViktor03.github.io
---
+++ projekt2/js/main.js
@@ -0,0 +1,6 @@
+var kepek=["nagykep/nagykep1","nagykep/nagykep2","nagykep/nagykep3","nagykep/nagykep4"];	
+function csere(kepnev){
+//document.nagykep.src=kepnev;
+//document.getElementById("nagykep").src=kepnev;
+document.querySelector("#nagykep").src=kepnev;
+}
